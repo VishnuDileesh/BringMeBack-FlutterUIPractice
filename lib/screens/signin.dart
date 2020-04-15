e932b43e38d8e8deb80ded005865e56c89bf9467
Pheyonix <dev@vishnudileesh.com>
2020-04-15 18:31:39 +0530

    Add Password input and forgot password

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -69,6 +69,60 @@ class SignIn extends StatelessWidget{
 									),
 								),
 
+								SizedBox(height: 16.0),
+
+								TextField(
+									style: TextStyle(
+										color: Color(0xff888888),
+									),
+									keyboardType: TextInputType.emailAddress,
+									obscureText: true,
+									decoration: InputDecoration(
+										hintText: "Password",
+										hintStyle: TextStyle(
+											color: Color(0xff888888),
+										),
+										
+										enabledBorder: OutlineInputBorder(
+											borderSide: BorderSide(
+												color: Color(0xff888888),
+											),
+										),
+
+										focusedBorder: OutlineInputBorder(
+											borderSide: BorderSide(
+												color: Color(0xff888888),
+											),
+										),
+
+										focusedErrorBorder: OutlineInputBorder(
+											borderSide: BorderSide(
+												color: Color(0xff888888),
+											),
+										),
+
+
+									),
+								),
+
+								SizedBox(height: 8.0),
+
+								Align(
+									alignment: Alignment.topRight,
+									child: InkWell(
+										onTap: (){},
+										child:Text(
+										'Forgot Password',
+										style: TextStyle(
+											color: Color(0xff888888),
+										),
+									),
+									),
+								),
+
+
+
+
 							],
 						),
 					),
