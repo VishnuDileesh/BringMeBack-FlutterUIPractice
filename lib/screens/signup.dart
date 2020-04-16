6bb48b0a8f6d6f2c0a42e58171c32974f217ac71
Pheyonix <dev@vishnudileesh.com>
2020-04-16 09:00:07 +0530

    Finish Sign Up screen

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -5,227 +5,218 @@ import '../widgets/colors.dart';
 import '../widgets/bgfullscreen.dart';
 import '../widgets/button.dart';
 
-class SignUp extends StatelessWidget{
-	@override
-	Widget build(BuildContext context){
-		return Material(
-			child: Scaffold(
-				extendBodyBehindAppBar: true,
-				backgroundColor: Colors.transparent,
-				appBar: AppBar(
-					leading: IconButton(
-						icon: Icon(
-							Icons.arrow_back,
-						),
-						onPressed: (){
-							Navigator.pop(context);
-						},
-					),
-					title: Text(
-						'Sign Up',
-					),
-					centerTitle: true,
-					backgroundColor: Colors.transparent,
-					elevation: 0.0,
-				),
-				body: Stack(
-				children: <Widget>[
-					
-					BGFullScreen(),
+//screens import
+import 'signin.dart';
 
-					SafeArea(
-						child: Container(
-							padding: EdgeInsets.symmetric(horizontal: 16.0),
-							child: Column(
-							mainAxisAlignment: MainAxisAlignment.center,
-							children: <Widget>[
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Text(
+            'Sign Up',
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+				resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: <Widget>[
+            BGFullScreen(),
+            Center(
+              child: SingleChildScrollView(
+                child: SafeArea(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.only(left: 12.0, right: 12.0),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(3.0),
+                                  border: Border.all(
+                                    color: grey,
+                                  ),
+                                ),
+                                child: DropdownButtonHideUnderline(
+                                  child: DropdownButton<String>(
+                                      items: [
+                                        DropdownMenuItem<String>(
+                                          value: 'Volunteer',
+                                          child: Center(
+                                              child: Text('Volunteer',
+                                                  style: TextStyle(
+                                                    color: grey,
+                                                  ))),
+                                        ),
+																		DropdownMenuItem<String>(
+                                          value: 'Donor',
+                                          child: Center(
+                                              child: Text('Donor',
+                                                  style: TextStyle(
+                                                    color: grey,
+                                                  ))),
+                                        ),
 
-								TextField(
-									style: TextStyle(
-										color: Color(0xff888888),
-									),
-									keyboardType: TextInputType.emailAddress,
-									decoration: InputDecoration(
-										hintText: "Email",
-										hintStyle: TextStyle(
-											color: grey,
-										),
-										
-										enabledBorder: OutlineInputBorder(
-											borderSide: BorderSide(
-												color: grey,
-											),
-										),
-
-										focusedBorder: OutlineInputBorder(
-											borderSide: BorderSide(
-												color: grey,
-											),
-										),
-
-										focusedErrorBorder: OutlineInputBorder(
-											borderSide: BorderSide(
-												color: grey,
-											),
-										),
-
-
-									),
-								),
-
-								SizedBox(height: 16.0),
-
-								TextField(
-									style: TextStyle(
-										color: grey,
-									),
-									keyboardType: TextInputType.emailAddress,
-									obscureText: true,
-									decoration: InputDecoration(
-										hintText: "Password",
-										hintStyle: TextStyle(
-											color: grey,
-										),
-										
-										enabledBorder: OutlineInputBorder(
-											borderSide: BorderSide(
-												color: grey,
-											),
-										),
-
-										focusedBorder: OutlineInputBorder(
-											borderSide: BorderSide(
-												color: grey,
-											),
-										),
-
-										focusedErrorBorder: OutlineInputBorder(
-											borderSide: BorderSide(
-												color: grey,
-											),
-										),
-
-
-									),
-								),
-
-								SizedBox(height: 8.0),
-
-								Align(
-									alignment: Alignment.topRight,
-									child: InkWell(
-										onTap: (){},
-										child:Text(
-										'Forgot Password',
-										style: TextStyle(
-											color: grey,
-										),
-									),
-									),
-								),
-
-								SizedBox(height: 40.0,),
-
-								ButtonFull(
-									btn_text: 'Sign Up',
-									btn_color: pink,
-								),
-
-								SizedBox(height: 32.0),
-
-								Row(
-									mainAxisAlignment: MainAxisAlignment.center,
-									children: <Widget>[
-
-										Container(
-											margin: EdgeInsets.only(left: 10.0, right: 10.0),
-											color: grey,
-											height: 1,
-											width: 30.0,
-										),
-
-										Text(
-											'OR',
-											style: TextStyle(
-												color: grey,
-												fontSize: 16.0,
-											),
-										),
-
-
-										Container(
-											margin: EdgeInsets.only(left: 10.0, right: 10.0),
-											color: grey,
-											height: 1,
-											width: 30.0,
-										),
-
-									],
-								),
-
-								
-								Row(
-									children: <Widget>[
-										Expanded(
-										child: Button(
-											btn_text: 'Facebook',
-											btn_color: Color(0xff3B5998),
-										),
-										),
-
-										SizedBox(width: 10.0),
-
-
-										Expanded(
-										child: Button(
-											btn_text: 'Google+',
-											btn_color: Color(0xffDB4A39),
-										),
-										),
-									],
-								),
-
-								SizedBox(height: 60.0),
-
-
-								Row(
-									mainAxisAlignment: MainAxisAlignment.center,
-									children: <Widget>[
-									
-										Text(
-											"Don't have an account?",
-											style: TextStyle(
-												color: grey,
-												fontSize: 16.0,
-											),
-										),
-
-										SizedBox(width: 3.0),
-
-										InkWell(
-											onTap: (){},
-											child: Text(
-												'Sign up',
-												style: TextStyle(
-													color: pink,
-													fontSize: 16.0,
-													decoration: TextDecoration.underline,
-												),
-											),
-										),
-
-									],
-								),
-
-
-							],
-						),
-					),
-					),
-
-				],
-			),
-				),
-		);
-	}
+                                      ],
+                                      hint: Text('Select Role',
+                                          style: TextStyle(
+                                            color: grey,
+                                          )),
+                                      onChanged: (_value) {
+                                        print(_value);
+                                      }),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16.0),
+                        TextField(
+                          style: TextStyle(
+                            color: Color(0xff888888),
+                          ),
+                          decoration: InputDecoration(
+                            hintText: "Username",
+                            hintStyle: TextStyle(
+                              color: grey,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: grey,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: grey,
+                              ),
+                            ),
+                            focusedErrorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        TextField(
+                          style: TextStyle(
+                            color: Color(0xff888888),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            hintText: "Email",
+                            hintStyle: TextStyle(
+                              color: grey,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: grey,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: grey,
+                              ),
+                            ),
+                            focusedErrorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        TextField(
+                          style: TextStyle(
+                            color: grey,
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: "Password",
+                            hintStyle: TextStyle(
+                              color: grey,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: grey,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: grey,
+                              ),
+                            ),
+                            focusedErrorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        ButtonFull(
+                          btn_text: 'Sign Up',
+                          btn_color: pink,
+                        ),
+                        SizedBox(height: 140.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Already have an account?",
+                              style: TextStyle(
+                                color: grey,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            SizedBox(width: 3.0),
+                            InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => SignIn()),
+                                );
+                              },
+                              child: Text(
+                                'Sign in',
+                                style: TextStyle(
+                                  color: pink,
+                                  fontSize: 16.0,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
